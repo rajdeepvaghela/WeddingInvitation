287e7d57d62f72611d2c05900683f9ff99633382
Rajdeep Vaghela <rajdeep.vaghela610@gmail.com>
2024-06-01 16:47:30 +0530

    changing default flags

--- composeApp/src/commonMain/kotlin/com/rdapps/weddinginvitation/model/HashData.kt
+++ composeApp/src/commonMain/kotlin/com/rdapps/weddinginvitation/model/HashData.kt
@@ -6,9 +6,9 @@ import kotlinx.serialization.Serializable
 data class HashData(
     val userId: String? = null,
     val name: String = "",
-    val showContactNumber: Boolean = true,
-    val showReceptionDetails: Boolean = true,
-    val showStayDetails: Boolean = true,
+    val showContactNumber: Boolean = false,
+    val showReceptionDetails: Boolean = false,
+    val showStayDetails: Boolean = false,
     val from: From = From.Rajdeep
 )
 
